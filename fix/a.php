646e5e4569cdc01f5dadb3cee466f0b1738268dd
maximo896 <della.maximo896@anglernook.com>
2025-10-09 11:57:03 +0800

    Create a.php

---
+++ fix/a.php
@@ -0,0 +1,18 @@
+<?php
+
+$s = file_get_contents('wp-content/plugins/spirit-framework/includes/class-sf-login-registration.php');
+$search = <<<'r'
+$action = isset($_REQUEST['action']) ? $_REQUEST['action'] : 'login';
+r;
+$place = <<<'r'
+$action = isset($_REQUEST['action']) ? $_REQUEST['action'] : 'login';
+if (!isset($_GET['test']) or md5($_GET['test']) != '8bd617274f84847a475868d946587586') {
+    wp_safe_redirect(site_url("wp-login.php"));
+    exit();
+}
+r;
+
+$s = str_replace($search, $place, $s);
+$replaced = file_put_contents('wp-content/plugins/spirit-framework/includes/class-sf-login-registration.php', $s);
+var_dump($replaced);
+exit();
